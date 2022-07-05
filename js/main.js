324e126d08b7cd1e0ad17ec829df470aab28a3e1
Osceola <gyula.burian@gmail.com>
2022-07-05 23:32:35 +0200

    nfhbadded

---
+++ js/main.js
@@ -0,0 +1,13 @@
+
+
+/* <script>
+  var v = document.getElementById("audio");
+
+  function My_VideoRate() {
+    v.playbackRate = 1;
+}
+
+  function Set_VideoRate() {
+    v.playbackRate = 1.3;
+}
+</script> */
